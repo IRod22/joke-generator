a5a872c3860b2f6e962ed93199fb8a8f824d3ec5
IRod22 <ivan18rod@yahoo.com>
2023-02-21 20:06:42 -0600

    :construction: add deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
-	},
-	preprocess: [
-		vitePreprocess({
-			postcss: true
-		})
-	],
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+		    base: process.env.NODE_ENV === "production" ? "/joke-generator" : "",
+		},
+		appDir: 'internal'
+	}
 };
 
 export default config;
