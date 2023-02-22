adfc66a8b01dabbf56097a241b6e6a3737933c29
IRod22 <ivan18rod@yahoo.com>
2023-02-21 22:09:57 -0600

    :bug: fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,7 +18,7 @@ const config = {
 			strict: false,
 		}),
 		paths: {
-		    base: process.env.NODE_ENV === "production" ? "/joke-generator" : "",
+		    base: dev ? "" : "/joke-generator",
 		},
 		appDir: 'internal'
 	}
