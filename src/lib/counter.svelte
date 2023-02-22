fac160d78eee8604b9cad6fafa116119de772e31
IRod22 <ivan18rod@yahoo.com>
2023-02-21 20:04:25 -0600

    :construction: add interactive component
    This is to test the production code

---
+++ src/lib/counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let value = 0
+
+    const incr = (amt: number) => () => {value += amt}
+</script>
+
+<span>
+    <button class="button" on:click={incr(-1)}>-</button>
+    {value}
+    <button class="button" on:click={incr(1)}>+</button>
+</span>
